0f72d8aafa96edc03a8a37962d6e85d850fa858c
Samir <samir.n@sip.az>
2022-03-16 17:56:59 +0400

    add table to admin`s answer page

--- src/admin/pages/answers/index.js
+++ src/admin/pages/answers/index.js
@@ -1,14 +1,121 @@
-import { useEffect, useMemo } from "react";
-import { connect } from "react-redux";
+import { useEffect, useMemo } from 'react';
+import { connect } from 'react-redux';
 
-import { SET_CURRENT_PAGE } from "../../../store/actions/actionTypes";
+import { AssignmentTurnedIn, Delete } from '@material-ui/icons';
 
-import "./style.scss";
+import { SET_CURRENT_PAGE } from '../../../store/actions/actionTypes';
+
+import './style.scss';
 
 function Answers(props) {
   return (
-    <div>
-      <p>Answers</p>
+    <div className="answersPage">
+      <div className="answerTable">
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Exam date</th>
+              <th>Question block</th>
+              <th>answers</th>
+              <th>delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Anar Ibrahimov</td>
+              <td>03.03.2022</td>
+              <td>
+                <span>2</span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <AssignmentTurnedIn style={{ color: '#006ade', fontSize: '22px' }} />
+                </span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <Delete style={{ color: 'red', fontSize: '22px' }} />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Senan Ibrahimov</td>
+              <td>03.03.2022</td>
+              <td>
+                <span>2</span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <AssignmentTurnedIn style={{ color: '#006ade', fontSize: '22px' }} />
+                </span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <Delete style={{ color: 'red', fontSize: '22px' }} />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Ivan Ibrahimov</td>
+              <td>03.03.2022</td>
+              <td>
+                <span>2</span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <AssignmentTurnedIn style={{ color: '#006ade', fontSize: '22px' }} />
+                </span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <Delete style={{ color: 'red', fontSize: '22px' }} />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>Cavad Ibrahimov</td>
+              <td>03.03.2022</td>
+              <td>
+                <span>2</span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <AssignmentTurnedIn style={{ color: '#006ade', fontSize: '22px' }} />
+                </span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <Delete style={{ color: 'red', fontSize: '22px' }} />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>Alex Ibrahimov</td>
+              <td>03.03.2022</td>
+              <td>
+                <span>2</span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <AssignmentTurnedIn style={{ color: '#006ade', fontSize: '22px' }} />
+                </span>
+              </td>
+              <td>
+                <span className="answersIcon">
+                  <Delete style={{ color: 'red', fontSize: '22px' }} />
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
@@ -20,8 +127,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    onChangeCurrentPage: (pageName) =>
-      dispatch({ type: SET_CURRENT_PAGE, payload: pageName }),
+    onChangeCurrentPage: (pageName) => dispatch({ type: SET_CURRENT_PAGE, payload: pageName }),
   };
 }
 
